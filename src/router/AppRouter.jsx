323f4ecd50adeed670e3a3aabe8454bc17ee1048
ehgus8 <68594605+ehgus8@users.noreply.github.com>
2025-06-30 09:21:39 +0900

    카카오 제거

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -8,9 +8,6 @@ import MainLayout from '../components/MainLayout';
 
 const AppRouter = () => {
   const { userRole } = useContext(AuthContext); // private 라우터를 이용하기 위해 추가(하준)
-  const KAKAO_REDIRECT_URI_PATH = new URL(
-    import.meta.env.VITE_KAKAO_REDIRECT_URI,
-  ).pathname;
 
   return (
     <Routes>
