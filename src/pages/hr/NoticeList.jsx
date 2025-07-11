886cc6604a19a8530f2ae5257eb1e2d9170a93e0
kimjiwon0450 <fresh04502@naver.com>
2025-07-11 15:18:11 +0900

    메인화면에서 부서별 공지사항 나오게 수정정

--- src/pages/hr/NoticeList.jsx
+++ src/pages/hr/NoticeList.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './NoticeList.scss';
 
 export default function NoticeList({ notices }) {
+  const navigate = useNavigate();
+
+  if (!notices || notices.length === 0) {
+    return <p className='no-notice'>공지사항이 없습니다.</p>;
+  }
+
   return (
-    <ul className='notice-list'>
-      {notices.map((notice, idx) => (
-        <li key={idx}>{notice}</li>
-      ))}
-    </ul>
+    <table className='notice-list'>
+      <tbody>
+        {notices.map((notice) => (
+          <tr
+            key={notice.id}
+            className='notice-item'
+            onClick={() => navigate(`/noticeboard/${notice.id}`)}
+          >
+            <td className='notice-title'>{notice.title}</td>
+            <td className='notice-author'>
+              {notice.employStatus === 'INACTIVE'
+                ? `${notice.name}(퇴사)`
+                : notice.name}
+            </td>
+            <td className='notice-date'>
+              {new Date(notice.createdAt).toLocaleDateString()}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
+
+
+
+
+
+// {notices.map((notice) => (
+//   <li
+//     key={notice.id}
+//     className='notice-item'
+//     onClick={() => navigate(`/noticeboard/${notice.id}`)}
+//     style={{ cursor: 'pointer' }}
+//   >
+//     📌 {notice.title}
+//   </li>
+// ))}
